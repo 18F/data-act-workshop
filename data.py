46e47c66081a34933203811d971537073be1d3c0
micahsaul <micah.saul@gsa.gov>
2015-03-26 10:53:32 -0400

    Add upload

--- data.py
+++ data.py
@@ -1,15 +1,19 @@
 import os
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask import send_from_directory
+from werkzeug import secure_filename
 
 import validator
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads/'
+
 test_data = {
   'agency': 'FWS',
-  'currentAmount': 500,
+  'currentAmount': 501,
   'initialAmount': 1000,
   'outlays': 500
 }
@@ -27,10 +31,26 @@ def validation():
     }
     return render_template('validation.html', context=context)
 
+@app.route("/submission/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        datafile = request.files['file']
+        filename = secure_filename(datafile.filename)
+        datafile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "Uploaded"
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
 if __name__ == "__main__":
 
     port = os.getenv('VCAP_APP_PORT', '5000')
 
     app.config['DEBUG'] = True
     app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.run(port=int(port), host='0.0.0.0')
